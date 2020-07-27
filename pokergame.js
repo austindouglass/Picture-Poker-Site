c04a35c9f4c23ddfc04cff7e907f6c73a4b14f61
imnotaustin22 <austindouglass22@gmail.com>
2020-07-27 15:09:49 -0700

    website initilization

---
+++ pokergame.js
@@ -0,0 +1,14 @@
+/*
+Reminder for things that will need to be done:
+-display number cards
+-create deck
+-make drawing cards random
+-selecting hold/draw
+-selecting bet
+-update for win
+-update for lose
+-update for draw
+-display cards
+-
+
+*/
