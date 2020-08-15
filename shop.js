ea532ac24a7578536213c6f3225454ecabe6916c
austindouglass <austindouglass22@gmail.com>
2020-08-15 14:09:52 -0700

    player can purchase a deck from the shop

--- shop.js
+++ shop.js
@@ -1,25 +1,64 @@
 
 //Copyright 2020, Austin Douglass, all rights reserved.
 
-var deckTypes = ["default", "numbers"], currentDeck = localStorage['dstyle'] || "default";
+//remember to store purchased decks!
 
+var decksOwned = ["default"], currentDeck = localStorage['dstyle'] || "default";
+var pMoney = 1000; //parseInt(localStorage['pm']) || 0;
+var allDecks = ["numbers"]; //every deck in the game (except default)
+var deckCost = {"default" : 0, "numbers" : 100};
+
+
+document.getElementById("playermoney").innerHTML = "Your Money: $" + pMoney.toString();
+displayShop();
 displayPlayerDecks();
 
+//player clicked to buy a deck
+function buyDeck(newDeck)    {
+    if(pMoney < deckCost[newDeck])  {
+        window.alert("You don't have enough money to purchase this item!")
+        return;
+    }
+    pMoney -= deckCost[newDeck];
+    //localStorage['pm'] = pMoney.toString();
+    document.getElementById("playermoney").innerHTML = "Your Money: $" + pMoney.toString();
+    decksOwned.push(newDeck);
+    displayShop();
+    displayPlayerDecks();
+}
+
+//shows decks available for purchase
+function displayShop()  {
+    var html = "", style = "style=\"cursor: pointer;\"";
+    for(i=0; i<allDecks.length; ++i)    {
+        if(!decksOwned.includes(allDecks[i]))   {
+            html += "<img src=\"images/decks/shop/" + allDecks[i] + "-buy.png\" onclick=\"buyDeck('";
+            html += allDecks[i] + "')\" " + style + "></img>";
+        }
+        else    {
+            html += "<img src=\"images/decks/shop/" + allDecks[i] + "-purchased.png\"></img>";
+        }
+    }
+    document.getElementById("dailyshop").innerHTML = html;
+}
+
+//equips the deck clicked on by player in your decks section
 function changeCurrentDeck(newDeck)   {
     currentDeck = newDeck;
     displayPlayerDecks();
     localStorage['dstyle'] = newDeck;
 }
 
+//shows your decks
 function displayPlayerDecks()   {
     var i, html = "", style = "style=\"cursor: pointer;\"";
-    for(i=0; i < deckTypes.length; ++i) {
-        if(deckTypes[i] != currentDeck) {
-            html += "<img src=\"images/decks/shop/" + deckTypes[i] + "-use.png\" onclick=\"changeCurrentDeck('";
-            html += deckTypes[i] + "')\" " + style + "></img>";
+    for(i=0; i < decksOwned.length; ++i) {
+        if(decksOwned[i] != currentDeck) {
+            html += "<img src=\"images/decks/shop/" + decksOwned[i] + "-use.png\" onclick=\"changeCurrentDeck('";
+            html += decksOwned[i] + "')\" " + style + "></img>";
         }
         else    {
-            html += "<img src=\"images/decks/shop/" + deckTypes[i] + "-in-use.png\"></img>";
+            html += "<img src=\"images/decks/shop/" + decksOwned[i] + "-in-use.png\"></img>";
         }
     }
     //console.log(html);
